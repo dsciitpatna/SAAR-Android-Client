2c443a302989ef1e644b25345445720ec630acf8
somenath sarkar <somenath1435@gmail.com>
2019-05-12 02:31:29 +0530

    Added two fragments

--- app/src/main/java/com/example/saar/MainActivity.java
+++ app/src/main/java/com/example/saar/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.saar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        displaySelectedScreen(R.id.nav_home);
     }
 
     @Override
@@ -77,29 +81,40 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        displaySelectedScreen(item.getItemId());
+        return true;
+    }
 
-        } else if (id == R.id.nav_about_us) {
+    private void displaySelectedScreen(int itemId) {
 
-        } else if (id == R.id.nav_timeline) {
+        //creating fragment object
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_team) {
-
-        } else if (id == R.id.nav_donate_now) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_contact_us) {
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.nav_about_us:
+                fragment = new AboutUsFragment();
+                break;
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_gallery:
+            case R.id.nav_timeline:
+            case R.id.nav_team:
+            case R.id.nav_donate_now:
+            case R.id.nav_share:
+            case R.id.nav_contact_us:
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
