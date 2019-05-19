a6b3c60a82ec0e8274afc82c9dc81e57f7f5dfb7
somenath sarkar <somenath1435@gmail.com>
2019-05-20 02:45:05 +0530

    Added login signup activity

---
+++ app/src/main/java/com/example/saar/LoginSignupActivity.java
@@ -0,0 +1,66 @@
+package com.example.saar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class LoginSignupActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+
+        //loading the default fragment
+        loadFragment(new LoginFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        //setting back button in toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.login:
+                fragment = new LoginFragment();
+                break;
+
+            case R.id.signup:
+                fragment = new SignUpFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_container, fragment);
+            ft.commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //handle back button action
+        onBackPressed();
+        return true;
+    }
+}
