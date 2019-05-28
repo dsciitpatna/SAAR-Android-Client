82637a811f95dfeb9b78237b16f264494090e1ee
somenath sarkar <somenath1435@gmail.com>
2019-05-28 16:41:07 +0530

    added event details page

--- app/src/main/java/com/example/saar/EventAdapter.java
+++ app/src/main/java/com/example/saar/EventAdapter.java
@@ -1,11 +1,16 @@
 package com.example.saar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.github.vipulasri.timelineview.TimelineView;
+
 import java.util.List;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.TimeLineViewHolder> {
@@ -22,6 +27,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.TimeLineView
     }
 
     public class TimeLineViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout parent_layout;
         public TextView title, date, location;
         public TimelineView timelineView;
 
@@ -29,6 +35,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.TimeLineView
             super(view);
             timelineView = view.findViewById(R.id.timeline);
             timelineView.initLine(viewType);
+            parent_layout = view.findViewById(R.id.parent_layout);
             title = view.findViewById(R.id.title);
             location = view.findViewById(R.id.location);
             date = view.findViewById(R.id.date);
@@ -44,10 +51,24 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.TimeLineView
     }
 
     @Override
-    public void onBindViewHolder(TimeLineViewHolder holder, int position) {
+    public void onBindViewHolder(final TimeLineViewHolder holder, final int position) {
         holder.title.setText(dataList.get(position).getTitle());
         holder.date.setText(dataList.get(position).getDate());
         holder.location.setText(dataList.get(position).getLocation());
+        holder.parent_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = holder.parent_layout.getContext();
+                Intent intent = new Intent(context, EventDetails.class);
+                intent.putExtra("title", dataList.get(position).getTitle());
+                intent.putExtra("description", dataList.get(position).getDescription());
+                intent.putExtra("location", dataList.get(position).getLocation());
+                intent.putExtra("date", dataList.get(position).getDate());
+                intent.putExtra("time", dataList.get(position).getTime());
+                intent.putExtra("image_url", dataList.get(position).getImage_url());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
