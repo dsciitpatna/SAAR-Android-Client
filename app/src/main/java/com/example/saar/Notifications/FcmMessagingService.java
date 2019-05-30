4165a6e03349a5644463bb60ffa33bf4d33a6855
Amartya Mondal <hayyoulistentome@gmail.com>
2019-05-31 00:33:46 +0530

    Image notification implemented

---
+++ app/src/main/java/com/example/saar/Notifications/FcmMessagingService.java
@@ -0,0 +1,87 @@
+package com.example.saar.Notifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.example.saar.MainActivity;
+import com.example.saar.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FcmMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getData().size() > 0) {
+            String title, message, img_url;
+            title = remoteMessage.getData().get("title");
+            message = remoteMessage.getData().get("message");
+            img_url = remoteMessage.getData().get("img_url");
+
+            String notification_channel_id = "SAAR";
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = new NotificationChannel(notification_channel_id, "Susi Notification", NotificationManager.IMPORTANCE_HIGH);
+
+                notificationChannel.setDescription("SAAR Notification");
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.enableVibration(true);
+
+
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            final NotificationCompat.Builder builder = new NotificationCompat.Builder(this, notification_channel_id);
+            builder.setAutoCancel(true);
+            builder.setContentTitle(title);
+            builder.setContentText(message);
+            builder.setContentIntent(pendingIntent);
+            builder.setSound(soundUri);
+            builder.setSmallIcon(R.drawable.saar_logo);
+
+            ImageRequest imageRequest = new ImageRequest(img_url, new Response.Listener<Bitmap>() {
+                @Override
+                public void onResponse(Bitmap response) {
+                    Log.d("KHANKI", "Testing- " + response);
+                    builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(response));
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationManager.notify(1, builder.build());
+
+
+                }
+            }, 0, 0, null, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("KHANKI", "Got error - " + error);
+                }
+            });
+            MySingleton.getmInstance(this).addToRequestQue(imageRequest);
+        }
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d("FCMTOKEN", "Token- " + token);
+    }
+}
