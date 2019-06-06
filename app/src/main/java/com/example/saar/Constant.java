beed0bac71f5a94ca6ece4c17c92806bc46894c7
Amartya Mondal <hayyoulistentome@gmail.com>
2019-06-06 13:38:51 +0530

    Login api along with data store implement

--- app/src/main/java/com/example/saar/Constant.java
+++ app/src/main/java/com/example/saar/Constant.java
@@ -4,9 +4,35 @@ public class Constant {
     public static final String FACEBOOK_LINK = "https://www.facebook.com/saar.iitp/";
     public static final String INSTAGRAM_LINK = "https://www.instagram.com/saar.iitp/";
     public static final String TWITTER_LINK = "https://twitter.com/saar_iitp";
-    public static final String LINKEDIN_LINK = "https://www.linkedin.com/company/saar-iitp";
+    public static final String SAAR_LINKEDIN_LINK = "https://www.linkedin.com/company/saar-iitp";
     public static final String YOUTUBE_LINK = "https://www.youtube.com/channel/UCoAb1YggffbfgVxQyXaZB-g";
     public static final String WEBSITE_LINK = "https://saar.iitp.ac.in/";
     public static final String DONATE_LINK = "https://www.onlinesbi.com/sbicollect/icollecthome.htm";
     public static final String BASE_URL = "https://atm1504.in";
+    public static final String LOGIN_URL = "http://192.168.0.101:8888/SAAR-Server/functions/login.php";
+    public static final String OTP_URL = "http://192.168.0.101:8888/SAAR-Server/functions/verifySignup.php";
+    public static final String SIGNUP_URL = "http://192.168.0.101:8888/SAAR-Server/functions/signup.php";
+
+    //Store user profile data
+    public static final String LOGIN_STATUS = "login_status";
+    public static final String EMAIL = "email";
+    public static final String ROLLNO = "rollno";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String PHONE = "phone";
+    public static final String FB_LINK = "fb_link";
+    public static final String LINKEDIN_LINK = "linkedin_link";
+    public static final String DOB = "dob";
+    public static final String GRADUATION_YEAR = "graduation_year";
+    public static final String DEGREE = "degree";
+    public static final String DEPARTMENT = "department";
+    public static final String EMPLOYEMENT_TYPE = "employment_type";
+    public static final String PRESENT_EMPLOYER = "present_employer";
+    public static final String DESIGNATION = "designation";
+    public static final String ADDRESS = "address";
+    public static final String COUNTRY = "country";
+    public static final String STATE = "state";
+    public static final String CITY = "city";
+    public static final String ACHIEVEMENTS = "achievements";
+
 }
--- app/src/main/java/com/example/saar/Login_SignUp/LoginFragment.java
+++ app/src/main/java/com/example/saar/Login_SignUp/LoginFragment.java
@@ -1,27 +1,60 @@
 package com.example.saar.Login_SignUp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.saar.Constant;
+import com.example.saar.MainActivity;
 import com.example.saar.OtpActivity;
 import com.example.saar.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import timber.log.Timber;
 
 public class LoginFragment extends Fragment {
 
     TextView verifyOTP;
+    EditText emailText, passwordText;
+    Button loginButton;
+    String email, password;
+    SharedPreferences.Editor sharedPreferenceEditor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView= inflater.inflate(R.layout.fragment_login, container, false);
-        verifyOTP=rootView.findViewById(R.id.verify_otp);
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        verifyOTP = rootView.findViewById(R.id.verify_otp);
+        emailText = rootView.findViewById(R.id.username_text);
+        passwordText = rootView.findViewById(R.id.password_text);
+        loginButton = rootView.findViewById(R.id.login_button);
+        sharedPreferenceEditor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
 
         return rootView;
     }
@@ -39,6 +72,107 @@ public class LoginFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDatas();
+            }
+        });
     }
 
+    private void getDatas() {
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getContext(), getString(R.string.credential_empty), Toast.LENGTH_LONG).show();
+        } else {
+            login();
+        }
+
+    }
+
+    private void login() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constant.LOGIN_URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Timber.d(response);
+                Log.d("KHANKI", response);
+                // otp_progress.setVisibility(View.GONE);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    int status = Integer.parseInt(jsonObject.getString("status"));
+                    if (status == 202) {
+                        Timber.d(getString(R.string.loggedin));
+                        Toast.makeText(getContext(), getString(R.string.loggedin), Toast.LENGTH_LONG).show();
+
+                        //Call the function to parse the JSON data
+                        storeUserInfo(jsonObject);
+
+                        Intent intent = new Intent(getContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+                    } else {
+                        Timber.d(getString(R.string.error_loggingin));
+                        JSONArray jsonArray = jsonObject.getJSONArray("messages");
+                        Toast.makeText(getContext(), jsonArray.toString(), Toast.LENGTH_LONG).show();
+                        Timber.d(jsonArray.toString());
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //otp_progress.setVisibility(View.GONE);
+                Timber.d(error.toString());
+            }
+
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                10000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        Request<String> data = requestQueue.add(stringRequest);
+    }
+
+    //A function that parses the success response, and stores the user data in the app.
+    private void storeUserInfo(JSONObject jsonObject) throws JSONException {
+        JSONObject mJsonObject = jsonObject.getJSONObject("messages");
+        sharedPreferenceEditor.putBoolean(Constant.LOGIN_STATUS, true);
+        sharedPreferenceEditor.putString(Constant.ROLLNO, mJsonObject.get(Constant.ROLLNO).toString());
+        sharedPreferenceEditor.putString(Constant.FIRST_NAME, mJsonObject.getString(Constant.FIRST_NAME));
+        sharedPreferenceEditor.putString(Constant.LAST_NAME, mJsonObject.getString(Constant.LAST_NAME));
+        sharedPreferenceEditor.putString(Constant.EMAIL, mJsonObject.getString(Constant.EMAIL));
+        sharedPreferenceEditor.putString(Constant.PHONE, mJsonObject.getString(Constant.PHONE));
+        sharedPreferenceEditor.putString(Constant.FB_LINK, mJsonObject.getString(Constant.FB_LINK));
+        sharedPreferenceEditor.putString(Constant.LINKEDIN_LINK, mJsonObject.getString(Constant.LINKEDIN_LINK));
+        sharedPreferenceEditor.putString(Constant.DOB, mJsonObject.getString(Constant.DOB));
+        sharedPreferenceEditor.putString(Constant.GRADUATION_YEAR, mJsonObject.getString(Constant.GRADUATION_YEAR));
+        sharedPreferenceEditor.putString(Constant.DEGREE, mJsonObject.getString(Constant.DEGREE));
+        sharedPreferenceEditor.putString(Constant.DEPARTMENT, mJsonObject.getString(Constant.DEPARTMENT));
+        sharedPreferenceEditor.putString(Constant.EMPLOYEMENT_TYPE, mJsonObject.getString(Constant.EMPLOYEMENT_TYPE));
+        sharedPreferenceEditor.putString(Constant.PRESENT_EMPLOYER, mJsonObject.getString(Constant.PRESENT_EMPLOYER));
+        sharedPreferenceEditor.putString(Constant.DESIGNATION, mJsonObject.getString(Constant.DESIGNATION));
+        sharedPreferenceEditor.putString(Constant.ADDRESS, mJsonObject.getString(Constant.ADDRESS));
+        sharedPreferenceEditor.putString(Constant.COUNTRY, mJsonObject.getString(Constant.COUNTRY));
+        sharedPreferenceEditor.putString(Constant.CITY, mJsonObject.getString(Constant.CITY));
+        sharedPreferenceEditor.putString(Constant.STATE, mJsonObject.getString(Constant.STATE));
+        sharedPreferenceEditor.putString(Constant.ACHIEVEMENTS, mJsonObject.getString(Constant.ACHIEVEMENTS));
+        sharedPreferenceEditor.apply();
+    }
 }
