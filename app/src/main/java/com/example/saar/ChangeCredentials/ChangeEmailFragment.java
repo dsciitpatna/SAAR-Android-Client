545f4f617c67f6b67050f09c542f69725920e3c6
Oindrila Bhadra <oindrilabhadra78@gmail.com>
2019-06-14 18:24:08 +0530

    Add scrollview

--- app/src/main/java/com/example/saar/ChangeCredentials/ChangeEmailFragment.java
+++ app/src/main/java/com/example/saar/ChangeCredentials/ChangeEmailFragment.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
