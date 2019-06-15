55190d65468edd4dc5342d938c97b8866ab72a04
Amartya Mondal <hayyoulistentome@gmail.com>
2019-06-16 01:19:15 +0530

    feat: Add videos of saar (#211)

--- app/src/main/java/com/example/saar/Video/VideosFragment.java
+++ app/src/main/java/com/example/saar/Video/VideosFragment.java
@@ -1,21 +1,29 @@
 package com.example.saar.Video;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.saar.R;
 
 public class VideosFragment extends Fragment {
 
+    ImageView video_1, video_2;
+    String video1Id= "1aW5r8-d2jQ";
+    String video2Id = "WdR0v0pJ4vY";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_videos, container, false);
+        video_1 = rootview.findViewById(R.id.video_1);
+        video_2 = rootview.findViewById(R.id.video_2);
+        return rootview;
     }
 
     @Override
@@ -23,6 +31,27 @@ public class VideosFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.videos);
+
+        video_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYoutube(video1Id);
+            }
+        });
+
+        video_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYoutube(video2Id);
+            }
+        });
+
+
+    }
+
+    public void openYoutube(String videoId){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://" + videoId));
+        startActivity(intent);
     }
 
 }
